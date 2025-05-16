1579c8a8c67916cfc6031581d6356714c12d218f
Michel116 <mvsh222222@gmail.com>
2025-05-16 23:57:10 +0300

    Delete src directory

--- src/hooks/useCart.ts
+++
@@ -1,4 +0,0 @@
-// This hook is defined and exported from src/contexts/CartContext.tsx
-// For modularity, one might create a separate file like this.
-// export { useCart } from '@/contexts/CartContext';
-export {};
